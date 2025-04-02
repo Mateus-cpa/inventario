8b6033be3fa30c472036628fcf95843b154065e5
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-04-02 07:11:36 -0300

    :sparkles: feat: botões de navegação

--- pages/status_base.py
+++ pages/status_base.py
@@ -1,26 +1,36 @@
 import streamlit as st
 import pandas as pd
-from levantamento import ler_base_processada
+from menu import ler_base_processada
 
 def pagina_principal(df):
+    """Configura as propriedades da página no Streamlit."""
+    st.set_page_config(
+        page_title='Status da Base Processada',
+        page_icon='📊',
+        layout='wide'
+    )
+    
     st.title('Status da Base Processada')
 
-    # Filtro por status
-    filtro_status = st.multiselect('Filtrar por colunas: ',df['status'].unique())
-    df = df[df['status'].isin(filtro_status)]
-
-    #KPIs
+    """Exibe os KPIs de quantidade de linhas e colunas."""
     col1, col2 = st.columns(2)
     col1.metric('Qtd. de Linhas', df.shape[0])
     col2.metric('Qtd. de Colunas', df.shape[1])
 
-    #dataframe
-    st.dataframe(df)
+    """Aplica filtro por status no DataFrame."""
+    filtro_status = st.multiselect('Filtrar por status: ', df['status'].unique())
+    if filtro_status:
+        df = df[df['status'].isin(filtro_status)]
 
-    #mostrar dtype de cada coluna
-    st.write('Filtrar colunas')
+
+    """Exibe o DataFrame e informações sobre os tipos de dados."""
+    st.dataframe(df)
+    st.subheader('Filtrar colunas')
     coluna = st.selectbox('Selecione a coluna', df.columns)
     st.write(f'Tipo de dado da coluna {coluna}: {df[coluna].dtype}')
+
+
+    
 
 if __name__ == '__main__':
     CAMINHO_ARQ_PROCESSADO = 'data_bronze/lista_bens-processado.csv'
