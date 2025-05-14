492d87cce45aefd6399ff64b65bd66c1bb29e99a
Mateus Pinheiro <mateus.mcpa@gmail.com>
2025-05-14 10:48:27 -0300

    :sparkles: feat: teste sql adicionando incrementalmente

--- sql.py
+++ sql.py
@@ -26,14 +26,26 @@ engine = create_engine("sqlite:///teste_levantamento.db", echo=True)
 #engine = create_engine(f"postgresql+psycopg2://{usuario}:{senha}@host:5432/inventario", echo=True)
 #print("Conexão com o PostgreSQL estabelecida com sucesso!")
 
-#conn =
+
+
+locais = ["Local A", "Local B", "Local C", "Local D", "Local E"]
 
 SQLModel.metadata.create_all(engine)
 
-locais = ["Local A", "Local B", "Local C", "Local D", "Local E"]
-patrimonio = 2010220039
-for i in range (0, 10):
-    x= i+i
+#pegar o ultimo patrimonio em teste_levantamento.db e salvar na variavel patrimonio
+connection = engine.connect()
+cursor = connection.connection.cursor()  # Use connection.connection to access the raw DB-API connection
+cursor.execute("SELECT num_tombamento FROM levantamento ORDER BY num_tombamento DESC LIMIT 1")
+patrimonio = cursor.fetchone()
+cursor.close()
+if patrimonio != None:
+    patrimonio = patrimonio[0]
+else:
+    patrimonio = 2010220001
+print(f"Ultimo patrimonio: {patrimonio}")
+
+for i in range (1, 20):
+    x= i
     levantamento = Levantamento(num_tombamento=patrimonio+x, 
                                 local_inventario=locais[randint(0, 4)], 
                                 horario_inventario=dt.now().strftime("%H:%M:%S"))
